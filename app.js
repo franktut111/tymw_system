a2fe16cdcd0405a66d2870eee22fbb7986834b15
franktut111 <franktut111@gmail.com>
2025-05-08 11:29:10 +0800

    change app.js

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ app.get('/',(req,res) => {
     res.send('Hello');
 })
 
+app.get('/book',(req,res)=>{
+    res.send('Get a book');
+})
+app.post('/book',(req,res)=>{
+    res.send('This is book page');
+})
+app.delete('/book',(req,res)=>{
+    res.send('Delete the book');
+})
+
 app.listen(port,() => {
-    console.log('listen on port localhost:3000');
+    console.log(`listen on port ${port}`);
 })
